62af344a7062ecac91663c50f7849c23c2d6717c
qwerty1213 <3031752913@qq.com>
2017-08-01 08:45:42 +0800

    注册登录忘记密码缓存

---
+++ app/src/main/java/com/example/administrator/pandatvsecond/model/bean/PersonRegister.java
@@ -0,0 +1,18 @@
+package com.example.administrator.pandatvsecond.model.bean;
+
+/**
+ * Created by xingge on 2017/7/27.
+ */
+
+public class PersonRegister {
+
+    private String msg;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
