c565b07967f638a3d37ae3b38d1636d1d27a347a
qwerty1213 <3031752913@qq.com>
2017-08-02 10:19:37 +0800

    整合代码

--- app/src/main/java/com/example/administrator/pandatvsecond/moudle/pandagg/GGFragment.java
+++ app/src/main/java/com/example/administrator/pandatvsecond/moudle/pandagg/GGFragment.java
@@ -26,8 +26,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
- * 滚滚视频
- */
+ * 滚滚视频但是规范化规范结核杆菌个 */
 
 public class GGFragment extends BaseFragment implements GGContract.View{
 
